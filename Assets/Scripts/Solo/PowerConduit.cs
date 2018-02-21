d6a8c13f5dfc3cbf7c8a75ecdfe552ee08421818
Billy-Desktop <wkarnchanapee@gmail.com>
2018-02-21 20:50:32 +0000

    WireBehaviour + probuilder

---
+++ Assets/Scripts/Solo/PowerConduit.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerConduit : ActivatableObject
+{
+    ActivatableObject parentOn;
+
+    private void Start()
+    {
+        parentOn = transform.parent.GetComponent<ActivatableObject>();
+    }
+    private void Update()
+    {
+        if (gameObject.tag == "wire-exit")
+        {
+            on = parentOn
+            if ()
+            // Check for timeout
+            if (timeout > timeoutTime)
+            {
+                on = false;
+                timeout = 0f;
+            }
+            else
+            {
+                timeout += Time.deltaTime;
+            }
+        }
+        else
+        {
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "wire-exit" && on == false)
+        {
+             = other.GetComponent<ActivatableObject>().on;
+        }
+    }
+}
---
+++ Assets/Scripts/WireBehaviour.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WireBehaviour : ActivatableObject
+{
+
+    Material m_Material;
+    [SerializeField] GameObject entry,exit;
+
+    private void Start()
+    {
+        m_Material = GetComponent<Renderer>().material;
+    }
+
+    private void Update()
+    {
+        if (on)
+        {
+            m_Material.color = Color.red;
+
+            // Check for timeout
+            if (timeout > timeoutTime)
+            {
+                on = false;
+                timeout = 0f;
+            } else
+            {
+                timeout += Time.deltaTime;
+            }
+        } else
+        {
+            m_Material.color = Color.white;
+        }
+    }
+
+    
+    private void OnTriggerStay(Collider other)
+    {
+
+        if (other.tag == "wire-exit" && on == false)
+        {
+ 
+            if (other.GetComponent<ActivatableObject>().on == true)
+            {
+                on = true;
+            }
+        }
+    }
+   
+}
