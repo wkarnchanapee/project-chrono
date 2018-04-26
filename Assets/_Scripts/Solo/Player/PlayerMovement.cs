feba230674325694db76d2215fe03d0d82a26967
Billy Karnchanapee <wkarnchanapee@gmail.com>
2018-04-26 22:37:46 +0100

    new movement code + sprinting and damping

--- Assets/_Scripts/Solo/Player/PlayerMovement.cs
+++ Assets/_Scripts/Solo/Player/PlayerMovement.cs
@@ -4,10 +4,13 @@ using System.Collections;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
-    float x, y, z;
+    [SerializeField] float x, y, z;
+    [SerializeField] float maxX, maxY, maxZ;
+    [SerializeField] float walkSpd, sprintSpd;
+    [SerializeField] float xDamp, zDamp;
     [SerializeField] float moveSpd, fallSpd, jumpHeight;
     CharacterController charCtrl;
-    Vector3 moveDir;
+    [SerializeField] Vector3 moveDir;
     // Use this for initialization
     void Start()
     {
@@ -17,8 +20,27 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        x = Input.GetAxis("Horizontal");
-        z = Input.GetAxis("Vertical");
+        x += Input.GetAxis("Horizontal");
+        z += Input.GetAxis("Vertical");
+
+        x = Mathf.Clamp(x, -maxX, maxX);
+        z = Mathf.Clamp(z, -maxZ, maxZ);
+
+        //damping back to 0
+        x = Mathf.Lerp(x, 0f, xDamp);
+        z = Mathf.Lerp(z, 0f, zDamp);
+
+
+        //sprint check
+        if (Input.GetButton("Sprint"))
+        {
+            maxX = sprintSpd;
+            maxZ = sprintSpd;
+        } else
+        {
+            maxX = walkSpd;
+            maxZ = walkSpd;
+        }
 
         //Gravity
         // Check Gravity
@@ -37,7 +59,9 @@ public class PlayerMovement : MonoBehaviour
 
         y = Mathf.Clamp(y, -fallSpd, jumpHeight);
 
+        
         moveDir = transform.TransformDirection(new Vector3(x, y, z) * moveSpd);
+        //moveDir = Vector3.Lerp(moveDir, Vector3.zero, 0.5f);
 
         charCtrl.Move(moveDir * Time.deltaTime);
     }
