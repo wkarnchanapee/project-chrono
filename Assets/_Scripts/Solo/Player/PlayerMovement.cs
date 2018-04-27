30491eac0a6002f1c94d6596dcabf25a283b536f
Billy Karnchanapee <wkarnchanapee@gmail.com>
2018-04-27 18:10:43 +0100

    Player Movement: in-air control, sprint checking, and new damping values.

--- Assets/_Scripts/Solo/Player/PlayerMovement.cs
+++ Assets/_Scripts/Solo/Player/PlayerMovement.cs
@@ -4,65 +4,112 @@ using System.Collections;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float x, y, z;
-    [SerializeField] float maxX, maxY, maxZ;
-    [SerializeField] float walkSpd, sprintSpd;
-    [SerializeField] float xDamp, zDamp;
-    [SerializeField] float moveSpd, fallSpd, jumpHeight;
+    float x, y, z;
+    float maxX, maxY, maxZ;
+    [Header("Set movement speed caps")]
+    [Tooltip("The walking speed cap.")]
+    [SerializeField] float walkSpd;
+    [Tooltip("The sprinting speed cap.")]
+    [SerializeField] float sprintSpd;
+
+
+    [Tooltip("Enable character control whilst not contacting ground?")]
+    [SerializeField] bool inAirMovement;
+
+
+    [Header("Damping")]
+    float xDamp;
+    float zDamp;
+    [Tooltip("The lerp to zero perecentage whilst mid-air.")]
+    [SerializeField] float airDamp;
+    [Tooltip("The lerp to zero percentage whilst on land.")]
+    [SerializeField] float landDamp;
+    [Tooltip("The amount to divide the current horizontal and vertical axis values by whilst mid-air.")]
+    [SerializeField] float inAirMoveModifier;
+
+    [Header("Jumping")]
+    [Tooltip("The speed at which the player falls at.")]
+    [SerializeField] float fallSpd;
+    [Tooltip("The maximum height of the jump.")]
+    [SerializeField] float jumpHeight;
+
+    [Tooltip("The multiplier that is applied to the final move direction vector. ")]
+    [SerializeField] float moveSpd;
+
     CharacterController charCtrl;
-    [SerializeField] Vector3 moveDir;
-    // Use this for initialization
+    Vector3 moveDir;
+    Camera cam;
+
     void Start()
     {
         charCtrl = GetComponent<CharacterController>();
+        cam = Camera.main;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        x += Input.GetAxis("Horizontal");
-        z += Input.GetAxis("Vertical");
 
+        // Get Input Axes
+        if (charCtrl.isGrounded)
+        {
+            x += Input.GetAxis("Horizontal");
+            z += Input.GetAxis("Vertical");
+        } else
+        {
+            x += Input.GetAxis("Horizontal") / inAirMoveModifier;
+            z += Input.GetAxis("Vertical") / inAirMoveModifier;
+        }
+        
+        // clamp forward and horizontal values.
         x = Mathf.Clamp(x, -maxX, maxX);
         z = Mathf.Clamp(z, -maxZ, maxZ);
 
-        //damping back to 0
+        // damping forward and horizontal values.
         x = Mathf.Lerp(x, 0f, xDamp);
         z = Mathf.Lerp(z, 0f, zDamp);
 
 
-        //sprint check
-        if (Input.GetButton("Sprint"))
-        {
-            maxX = sprintSpd;
-            maxZ = sprintSpd;
-        } else
-        {
-            maxX = walkSpd;
-            maxZ = walkSpd;
-        }
-
-        //Gravity
-        // Check Gravity
         if (charCtrl.isGrounded == true)
         {
+            // jump check
             if (Input.GetButtonDown("Jump"))
             {
                 y = jumpHeight;
 
             }
+
+            //sprint check
+            if (Input.GetButton("Sprint"))
+            {
+                maxX = sprintSpd;
+                maxZ = sprintSpd;
+
+            }
+            else
+            {
+                maxX = walkSpd;
+                maxZ = walkSpd;
+
+            }
+
+            // apply land damping speeds
+            xDamp = landDamp;
+            zDamp = landDamp;
         }
         else
         { 
+            // apply air damping speeds.
+            xDamp = airDamp;
+            zDamp = airDamp;
+
+            // gravity
             y -= fallSpd * Time.deltaTime;
         }
 
+        // gravity clamp.
         y = Mathf.Clamp(y, -fallSpd, jumpHeight);
-
         
         moveDir = transform.TransformDirection(new Vector3(x, y, z) * moveSpd);
-        //moveDir = Vector3.Lerp(moveDir, Vector3.zero, 0.5f);
-
         charCtrl.Move(moveDir * Time.deltaTime);
     }
 }
